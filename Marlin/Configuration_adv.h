9e91c16dcd3214233df70350fe92136be2f91f1c
stofstik <verstegen.daan@gmail.com>
2017-04-17 13:59:28 +0200

    increase thermal protection tolerance

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -18,8 +18,8 @@
  * Thermal Protection parameters
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
-  #define THERMAL_PROTECTION_PERIOD 40        // Seconds
-  #define THERMAL_PROTECTION_HYSTERESIS 4     // Degrees Celsius
+  #define THERMAL_PROTECTION_PERIOD     60    // Seconds
+  #define THERMAL_PROTECTION_HYSTERESIS 10    // Degrees Celsius
 
   /**
    * Whenever an M104 or M109 increases the target temperature the firmware will wait for the
@@ -27,7 +27,7 @@
    * degrees, the machine is halted, requiring a hard reset. This test restarts with any M104/M109,
    * but only if the current temperature is far enough below the target for a reliable test.
    */
-  #define WATCH_TEMP_PERIOD 16                // Seconds
+  #define WATCH_TEMP_PERIOD   16              // Seconds
   #define WATCH_TEMP_INCREASE 4               // Degrees Celsius
 #endif
 
